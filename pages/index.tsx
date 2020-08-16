8bd937c80c7e371b60791120473560bcfd91f160
zhenlong qin <qinzhenlong@totok.ai>
2020-08-16 14:44:29 +0400

    tempalte with scss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,21 @@
 import Link from "next/link"
+import Styles from "styles/Index.module.scss";
+
 export default function Home() {
   return (
-    <div>
-      <Link href="/share" as="/share">Share</Link>
+    <div className={Styles.index}>
+      <Link 
+      href="/share" 
+      as="/share">
+      <a className={Styles.link}>Go to share</a>
+      </Link>
+      <div className={Styles.warp}>
+        <Link 
+        href="/share" 
+        as="/share">
+        <a className={Styles.link}>Go to share</a>
+        </Link>
+      </div>
     </div>
   )
 }
